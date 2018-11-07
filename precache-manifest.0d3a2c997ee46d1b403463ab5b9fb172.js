c6492317951ccbbdfa804ef921972feaf9efcbc7
lopesamit <lopesamit@gmail.com>
2018-11-07 16:03:53 -0500

    Updates

---
+++ precache-manifest.0d3a2c997ee46d1b403463ab5b9fb172.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "0faacd911db2cf027449301152bd8ba4",
+    "url": "/static/media/vm-logo-primary.0faacd91.svg"
+  },
+  {
+    "revision": "6cdbd05eaf100644e2f197d849eff8c2",
+    "url": "/static/media/multi-email-cancel.6cdbd05e.svg"
+  },
+  {
+    "revision": "032abe26be65f1fb5438defe997c7ba5",
+    "url": "/static/media/ic-large-email-generic.032abe26.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f4ea7d05564c2f8bf507",
+    "url": "/static/js/main.f4ea7d05.chunk.js"
+  },
+  {
+    "revision": "4b5e55afd26fbf9e4711",
+    "url": "/static/js/1.4b5e55af.chunk.js"
+  },
+  {
+    "revision": "f4ea7d05564c2f8bf507",
+    "url": "/static/css/main.9de55e93.chunk.css"
+  },
+  {
+    "revision": "4b5e55afd26fbf9e4711",
+    "url": "/static/css/1.d138a09c.chunk.css"
+  },
+  {
+    "revision": "5ed22946059d539298525d047a2732b4",
+    "url": "/index.html"
+  }
+];
